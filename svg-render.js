0c5a6a0394020c986708124c2da4e15064694751
ETHproductions <eth3792@gmail.com>
2024-07-19 19:57:02 -0400

    add cells, hover to change color

--- svg-render.js
+++ svg-render.js
@@ -22,19 +22,57 @@ export function renderGrid(grid, scale) {
 
     svg.setAttribute("width", width);
     svg.setAttribute("height", height);
-    console.log(grid.verts);
+    
+    const cellsBase = document.createElementNS(ns, "g");
+    cellsBase.setAttribute("id", "cells-base");
+    svg.appendChild(cellsBase);
+
+    const edgesBase = document.createElementNS(ns, "g");
+    edgesBase.setAttribute("id", "edges-base");
+    svg.appendChild(edgesBase);
+
+    const edgesHitbox = document.createElementNS(ns, "g");
+    edgesHitbox.setAttribute("id", "edges-hitbox");
+    svg.appendChild(edgesHitbox);
+
+    console.log(grid.cells);
 
     for (let edge of grid.edges) {
+        let vert1 = grid.verts[edge.fromVert];
+        let vert2 = grid.verts[edge.toVert];
+
         let line = document.createElementNS(ns, "line");
         line.setAttributeNS(null, "stroke", "black");
         line.setAttributeNS(null, "stroke-width", edge.isEdgeOfGrid || (edge.leftCell != null && edge.rightCell != null && grid.cells[edge.leftCell].area_id != grid.cells[edge.rightCell].area_id) ? 3 : 1);
         line.setAttributeNS(null, "stroke-linecap", "square");
-        let vert1 = grid.verts[edge.fromVert];
         line.setAttributeNS(null, "x1", originX + vert1.rpos.x * scale);
         line.setAttributeNS(null, "y1", originY + vert1.rpos.y * scale);
-        let vert2 = grid.verts[edge.toVert];
         line.setAttributeNS(null, "x2", originX + vert2.rpos.x * scale);
         line.setAttributeNS(null, "y2", originY + vert2.rpos.y * scale);
-        svg.appendChild(line);
+        edgesBase.appendChild(line);
+        
+        let hitbox = document.createElementNS(ns, "line");
+        hitbox.setAttributeNS(null, "stroke", "transparent");
+        hitbox.setAttributeNS(null, "stroke-width", 5);
+        hitbox.setAttributeNS(null, "x1", originX + vert1.rpos.x * scale);
+        hitbox.setAttributeNS(null, "y1", originY + vert1.rpos.y * scale);
+        hitbox.setAttributeNS(null, "x2", originX + vert2.rpos.x * scale);
+        hitbox.setAttributeNS(null, "y2", originY + vert2.rpos.y * scale);
+
+        hitbox.onmouseover = (e) => line.setAttributeNS(null, "stroke", "red");
+        hitbox.onmouseout = (e) => line.setAttributeNS(null, "stroke", "black");
+        edgesHitbox.appendChild(hitbox);
+    }
+
+    for (let cell of grid.cells) {
+        let path = document.createElementNS(ns, "path");
+        path.setAttributeNS(null, "fill", "white");
+        path.setAttributeNS(null, "d", cell.verts.map((v, i) => {
+            let {x, y} = grid.verts[v].rpos;
+            return (i > 0 ? "L " : "M ") + [originX + x * scale, originY + y * scale]
+        }).join(" ") + " Z");
+        path.onmouseover = (e) => path.setAttributeNS(null, "fill", "lightgrey");
+        path.onmouseout = (e) => path.setAttributeNS(null, "fill", "white");
+        cellsBase.appendChild(path);
     }
 }
