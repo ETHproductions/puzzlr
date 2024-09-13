e8e030ebaf151322e47e64e76cc0388b83c9f106
ETHproductions <eth3792@gmail.com>
2024-09-13 13:54:35 -0400

    convert constraints
    eventually I will want a better system but like, let's just get it working how it was for now

--- src/base/puzzle-types/norinori.ts
+++ src/base/puzzle-types/norinori.ts
@@ -1,30 +1,34 @@
-import Puzzle from '../Puzzle';
-import { SUM_EQUALS, SUM_EQUALS_IF } from '../base/generic-constraints.js';
-import SquareGrid from '../grids/SquareGrid';
+import Puzzle from "../Puzzle";
+import { SUM_EQUALS, SUM_EQUALS_IF } from "../generic-constraints";
+import SquareGrid from "../grids/SquareGrid";
 
 class NorinoriPuzzle extends Puzzle {
-    get type() { return 'norinori'; }
-    get renderSettings() { return { defaultScale: 30, funcs: ["edgearea", "binarygrey"] }; }
+  get type() {
+    return "norinori";
+  }
+  get renderSettings() {
+    return { defaultScale: 30, funcs: ["edgearea", "binarygrey"] };
+  }
 
-    grid: SquareGrid;
-    areas: { x: number, y: number }[][];
-    constructor({ areas }: { areas: { x: number, y: number }[][] }) {
-        const grid = SquareGrid.fromAreas(areas);
-        super(grid);
-        this.grid = grid;
-        this.areas = areas;
+  grid: SquareGrid;
+  areas: { x: number; y: number }[][];
+  constructor({ areas }: { areas: { x: number; y: number }[][] }) {
+    const grid = SquareGrid.fromAreas(areas);
+    super(grid);
+    this.grid = grid;
+    this.areas = areas;
 
-        for (const area of areas) {
-            const cells = [];
-            for (const pos of area) {
-                const cell = this.grid.cellmap.get2D(pos.x, pos.y);
-                cells.push(cell);
-                this.addVariable(cell, [0, 1]);
-                this.addConstraint(SUM_EQUALS_IF, [cell, ...cell.adjacentEdge], 1, 1);
-            }
-            this.addConstraint(SUM_EQUALS, cells, 2);
-        }
+    for (const area of areas) {
+      const cells = [];
+      for (const pos of area) {
+        const cell = this.grid.cellmap.get2D(pos.x, pos.y);
+        cells.push(cell);
+        this.addVariable(cell, [0, 1]);
+        this.addConstraint(SUM_EQUALS_IF, [cell, ...cell.adjacentEdge], 1, 1);
+      }
+      this.addConstraint(SUM_EQUALS, cells, 2);
     }
+  }
 }
 
 export default NorinoriPuzzle;
